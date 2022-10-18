afd36b2e5a4bde10342b12f65d06de106c1e3596
Konstantin Runov <runebone1@gmail.com>
2022-10-18 15:11:43 +0300

    postgres db: add on delete cascade

--- postgres_db/create_tables.sql
+++ postgres_db/create_tables.sql
@@ -5,13 +5,13 @@ CREATE TABLE IF NOT EXISTS stores (
 
 CREATE TABLE IF NOT EXISTS addresses (
     id       UUID                       NOT NULL PRIMARY KEY,
-    store_id UUID REFERENCES stores(id) NOT NULL,
+    store_id UUID REFERENCES stores(id) ON DELETE CASCADE NOT NULL,
     address  VARCHAR(255)               NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS packages (
     id            UUID                          NOT NULL PRIMARY KEY,
-    address_id    UUID REFERENCES addresses(id) NOT NULL,
+    address_id    UUID REFERENCES addresses(id) ON DELETE CASCADE NOT NULL,
     description   VARCHAR(255)                  NOT NULL,
     pickup_before TIMESTAMP                     NOT NULL,
     amount        INT                           NOT NULL,
@@ -19,12 +19,14 @@ CREATE TABLE IF NOT EXISTS packages (
     );
 
 CREATE TABLE IF NOT EXISTS orders (
-    id UUID NOT NULL PRIMARY KEY
+    id            UUID   NOT NULL PRIMARY KEY,
+    chat_id       BIGINT NOT NULL,
+    random_number BIGINT NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS order_packages (
-    order_id   UUID REFERENCES orders(id)   NOT NULL,
-    package_id UUID REFERENCES packages(id) NOT NULL
+    order_id   UUID REFERENCES orders(id)   ON DELETE CASCADE NOT NULL,
+    package_id UUID REFERENCES packages(id) ON DELETE CASCADE NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS partners (
@@ -32,16 +34,16 @@ CREATE TABLE IF NOT EXISTS partners (
     );
 
 CREATE TABLE IF NOT EXISTS partner_addresses (
-    partner_id BIGINT REFERENCES partners(chat_id) NOT NULL,
-    address_id UUID   REFERENCES addresses(id)     NOT NULL
+    partner_id BIGINT REFERENCES partners(chat_id) ON DELETE CASCADE NOT NULL,
+    address_id UUID   REFERENCES addresses(id)     ON DELETE CASCADE NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS partner_packages (
-    partner_id BIGINT REFERENCES partners(chat_id) NOT NULL,
-    package_id UUID   REFERENCES packages(id)      NOT NULL
+    partner_id BIGINT REFERENCES partners(chat_id) ON DELETE CASCADE NOT NULL,
+    package_id UUID   REFERENCES packages(id)      ON DELETE CASCADE NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS partner_orders (
-    partner_id BIGINT REFERENCES partners(chat_id) NOT NULL,
-    order_id   UUID   REFERENCES orders(id)        NOT NULL
+    partner_id BIGINT REFERENCES partners(chat_id) ON DELETE CASCADE NOT NULL,
+    order_id   UUID   REFERENCES orders(id)        ON DELETE CASCADE NOT NULL
     );
